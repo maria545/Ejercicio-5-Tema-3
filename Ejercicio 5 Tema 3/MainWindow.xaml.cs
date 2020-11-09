7950b5cabe055da721e7a06a2cef4b0fee40204b
Maria <Maria@DESKTOP-G07TP2O>
2020-11-09 18:25:06 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio 5 Tema 3/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Ejercicio_5_Tema_3
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {          
+            Button boton = (Button)sender;
+            EscribirTextBlock.Text = EscribirTextBlock.Text + Convert.ToString(boton.Tag);
+        }
+    }
+}
